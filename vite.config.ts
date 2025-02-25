63d4ea1111d3be61b92b6af409a92fcae0d1d952
hairyf <wwu710632@gmail.com>
2025-02-25 17:24:14 +0800

    chore: update build options

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,8 @@ export default defineConfig({
   },
 
   build: {
+    sourcemap: true,
+    minify: false,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'MoonchainMapper',
